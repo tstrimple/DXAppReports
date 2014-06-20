77e8a09f63ee834c8f6181f76233ebaf35af18db
Timothy Strimple <tim@tstrimple.com>
2014-06-20 10:58:34 -0700

    more cleanup

---
+++ models/app.js
@@ -0,0 +1,86 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var moment = require('moment-timezone');
+var util = require('util');
+var StoreLink = require('./link');
+var StoreRating = require('./rating');
+var regions = require('../data/regions');
+var async = require('async');
+var crawler = require('../crawler/');
+var debug = require('debug')('appreports:app');
+
+var schema = new Schema({
+	storeId: String,
+  name: String,
+  image: String,
+  primaryUrl: String,
+  bitly: String,
+  formatUrl: String,
+  platform: String,
+  segment: String,
+  baseline: Number
+});
+
+schema.methods.expandStoreLinks = function(done) {
+  debug('total regions to verify', regions.length);
+  async.eachLimit(regions, 20, function(region, nextRegion) {
+    var url = this.getUrl(region);
+    crawler.getStatus(url, function(status) {
+      var data = {
+        storeId: this.storeId,
+        region: region
+      };
+
+      StoreLink.findOneAndUpdate(data, data, { upsert: true }, function(err, doc) {
+        doc.status = status;
+        doc.name = this.name;
+        doc.segment = this.segment;
+        doc.processedAt = moment().tz('America/Los_Angeles').toDate();
+        doc.platform = this.platform;
+        doc.primaryUrl = this.primaryUrl;
+        doc.url = url;
+        doc.bitly = this.bitly;
+        doc.baseline = this.baseline;
+
+        debug('upserting StoreLink', doc);
+        doc.save();
+        nextRegion();
+      }.bind(this));
+    }.bind(this));
+  }.bind(this), done);
+}
+
+schema.methods.parseUrl = function(url) {
+	if(url.indexOf('windowsphone.com') > 0) {
+		this.platform = 'phone';
+  } else {
+  	this.platform = 'client';
+  }
+
+  this.storeId = url.substr(url.length - 36);
+  this.primaryUrl = url;
+  this.formatUrl = url.replace('en-us', '%s');
+}
+
+schema.methods.getUrl = function(region) {
+	return util.format(this.formatUrl, region);
+}
+
+schema.statics.getRatingsForYesterday = function(storeId, callback) {
+  StoreRating.getForDate(storeId, StoreRating.yesterday(), callback);
+}
+
+schema.statics.getRatingsForToday = function(storeId, callback) {
+  StoreRating.getForDate(storeId, StoreRating.today(), callback);
+}
+
+schema.statics.getRatingChange = function(storeId, callback) {
+  this.getRatingsForToday(storeId, function(err, today) {
+    this.getRatingsForYesterday(storeId, function(err, yesterday) {
+      debug('icanhaschange?', today, yesterday);
+      callback(err, today.totalRatings - yesterday.totalRatings);
+    });
+  }.bind(this));
+}
+
+module.exports = mongoose.model('App', schema);
---
+++ models/link.js
@@ -0,0 +1,22 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var schema = new Schema({
+  storeId: String,
+  name: String,
+  platform: String,
+  bitly: String,
+  primaryUrl: String,
+  url: String,
+  region: String,
+  segment: String,
+  status: String,
+  processedAt: Date,
+  baseline: Number
+});
+
+schema.statics.upsert = function() {
+
+}
+
+module.exports = mongoose.model('Link', schema);
