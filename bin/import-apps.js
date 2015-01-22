d47231d8b7fd39c2671aa5f859e06b909431bfbf
Timothy Strimple <tim@tstrimple.com>
2015-01-22 04:18:57 -0600

    added import script

---
+++ bin/import-apps.js
@@ -0,0 +1,63 @@
+var parser = require('csv-parse')();
+var fs = require('fs');
+var crawler = require('app/crawler');
+var App = require('models/app');
+var debug = require('debug')('appreports:import');
+var async = require('async');
+var convertDateString = require('app/date').convertDateString;
+require('app/db').connect();
+
+
+if(process.argv.length < 3) {
+  return console.error('You must supply the path to the csv file to import.');
+}
+
+var importFile = process.argv[2];
+
+if (!fs.existsSync(importFile)) {
+  return console.error('Could not find the file specified.');
+}
+
+var importStream = fs.createReadStream(importFile);
+
+var apps = [];
+
+parser.on('readable', function() {
+  while(record = parser.read()) {
+    var appData = {
+      storeId: record[3],
+      datePublished: convertDateString(record[0]),
+      lastUpdated: convertDateString(record[0]),
+      name: record[2],
+      primaryUrl: ''
+    };
+
+    apps.push(appData);
+  }
+});
+
+parser.on('error', function(err) {
+  debug('error', err.message);
+});
+
+parser.on('finish', function(err) {
+  async.eachSeries(apps, function(app, nextApp) {
+    crawler.getStoreUrl(app.storeId, function(err, url) {
+      if(err) {
+        return debug('error importing ' + app.name, err);
+      }
+
+      app.primaryUrl = url;
+
+      App.createFromData(app, function() {
+        debug('imported', app.name);
+        nextApp();
+      });
+    });
+  }, function(err) {
+    debug('finished', err);
+    process.exit();
+  });
+});
+
+importStream.pipe(parser);
